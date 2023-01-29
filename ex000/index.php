ad8ae80799424211869d343a6ea1588a282bce51
brendonfeitosa <brendonfeitosa@hotmail.com>
2023-01-29 12:42:44 -0300

    Initial commit

---
+++ ex000/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldição</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Olá Mundo! \u{1F30E}";//o código \u{1F30E} é para colocar o mapa, se eu trocar o código dentro das chaves posso colocar outro emoji
+        ?>
+    </h1>
+    <p>Vamos tentar nos livrar da maldição</p>
+</body>
+</html>
